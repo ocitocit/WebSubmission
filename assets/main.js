404e4bba50941547fba8c392057bde2e62e6baf4
ocrit28 <ocritocrit@gmail.com>
2022-06-30 23:12:07 +0700

    FLEX NYOBAIN NAV

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+// ========toggle light and dark=========
+const checkbox = document.getElementById("checkbox");
+
+checkbox.addEventListener("change", () => {
+  // theme ny
+  document.body.classList.toggle("dark");
+});
+// =========================================
