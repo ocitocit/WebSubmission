3ed7fa7c9c67f6aa204232b49ff5097f8da104c4
ocrit28 <ocritocrit@gmail.com>
2022-07-15 22:22:46 +0700

    2 section kelar

--- assets/main.js
+++ assets/main.js
@@ -5,4 +5,21 @@ checkbox.addEventListener("change", () => {
   // theme ny
   document.body.classList.toggle("dark");
 });
-// =========================================
+// ===================skills======================
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
